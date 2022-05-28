b371ce54267dc7a0d7327a4fc489e3c970db0b3b
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-28 14:47:16 -0500

    clean code and add fetch the geolocation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,43 @@
 var cityFormEl = document.querySelector("#city-form");
 var locationInputEl = document.querySelector("#location");
-var error = document.querySelector(".modal-error");
+
+var containerWeather = document.querySelector("#weather-container");
+
+var loadWeather = function () {
+  containerWeather.style.visibility = "hidden";
+};
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
   var cityName = locationInputEl.value.trim();
   if (cityName) {
-    getCityData(username);
+    getCityData(cityName);
     cityName.value = "";
   } else {
-    alert("Please enter a City username");
+    alert("Please enter a City");
   }
   console.log(event);
 };
 
-error.addEventListener("click", closeError);
-
-var getCityData = function () {};
+var getCityData = function (cityName) {
+  var key = "98cc685a511e4cdddd0311ce79a8e339";
+  fetch(
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+      cityName +
+      " &appid=" +
+      key
+  )
+    .then(function (resp) {
+      return resp.json();
+    }) // Convert data to json
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (e) {
+      console.log(e);
+    });
+};
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
+
+loadWeather();
