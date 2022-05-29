654cf82a3969b98681ddad55f56d6c10e8e0442c
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-28 23:32:41 -0500

    adding forecast 5days

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,20 +28,22 @@ var getCityData = function (cityName) {
     " &appid=" +
     key;
 
-  fetch(url)
-    .then(resp => {
-      if (!resp.ok) throw new Error(resp.statusText);
-      return resp.json();
-    })
-    .then(data => {
-      var lat = data[0].lat;
-      var lon = data[0].lon;
-      cityGlobal = data[0].name;
-      cityWeather(lat, lon, cityGlobal);
-    })
-    .catch(function (e) {
-      console.log(e);
-    });
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        if (data.length != 0) {
+          var lat = data[0].lat;
+          var lon = data[0].lon;
+          cityGlobal = data[0].name;
+          cityWeather(lat, lon, cityGlobal);
+        } else {
+          alert("Please check name of the city");
+        }
+      });
+    } else {
+      alert("Please check name of the city");
+    }
+  });
 };
 
 var cityWeather = function (lat, lon, cityName) {
@@ -59,17 +61,21 @@ var cityWeather = function (lat, lon, cityName) {
     units +
     "&lang=" +
     lang;
-  //fetch the weather
-  fetch(url)
-    .then(resp => {
-      if (!resp.ok) throw new Error(resp.statusText);
-      return resp.json();
-    })
-    .then(data => {
-      console.log(data);
-      displayWeather(data, cityName);
-    })
-    .catch(console.err);
+
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        if (data.length != 0) {
+          console.log(data);
+          displayWeather(data, cityName);
+        } else {
+          alert("Error to get the city weather");
+        }
+      });
+    } else {
+      alert("Error to get the city weather");
+    }
+  });
 };
 
 var displayWeather = function (cityData, cityName) {
@@ -112,7 +118,72 @@ var displayWeather = function (cityData, cityName) {
       '<span class="uvi badge bg-danger">' + cityData.current.uvi + "</span>"
     );
   }
+
+  cityForecast(cityData.daily);
 };
+
+var cityForecast = function (dailyData) {
+  //previous searchs elimanate those elements
+  if ($("#rowForecast div").length > 0) {
+    $("#rowForecast div").remove();
+    // $('#section-cards br').remove();
+  }
+  $("#rowForecast").append('<div class="row" id="rowNew">');
+  //forecast for 5 days
+  for (var i = 0; i < 5; i++) {
+    //$("#rowForecast").append('<div class="row" id="row' + i + '">');
+    $("#rowNew").append(
+      '<div class="col-lg-2 col-md-3 col-6" id="col' + i + '">'
+    );
+    $("#col" + i).append(
+      '<div class="card card-default" id="colCard' + i + '">'
+    );
+    $("#colCard" + i).append(
+      '<div class="card text-white bg-primary" id="card' + i + '">'
+    );
+    $("#card" + i).append('<div class="card-block" id="block' + i + '">');
+
+    var date = new Date(dailyData[i].dt * 1000);
+    var dateForecast = date.toDateString().slice(0, -5);
+
+    $("#block" + i).append(
+      '<h4 class="card-title" id="date>' + i + '">' + dateForecast
+    );
+
+    var iconWeather =
+      "https://openweathermap.org/img/wn/" +
+      dailyData[i].weather[0].icon +
+      ".png";
+
+    $("#block" + i).append('<img src="' + iconWeather + '" />');
+
+    $("#block" + i).append(
+      '<p class="card-text">Temp Max: ' +
+        dailyData[i].temp.max +
+        " <span></span> &deg;F</p>"
+    );
+
+    $("#block" + i).append(
+      '<p class="card-text">Temp Min: ' +
+        dailyData[i].temp.min +
+        " <span></span> &deg;F</p>"
+    );
+
+    $("#block" + i).append(
+      '<p class="card-text">Wind : ' +
+        dailyData[i].wind_speed +
+        " <span></span> MPH"
+    );
+
+    $("#block" + i).append(
+      '<p class="card-text">Humidity : ' +
+        dailyData[i].humidity +
+        " <span></span> %"
+    );
+  }
+};
+{
+}
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
 
