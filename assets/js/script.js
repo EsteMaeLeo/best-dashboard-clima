159ba4b81b9c5372ef4c14cbce298e06a3a55cc3
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-28 19:09:23 -0500

    adding information current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var cityFormEl = document.querySelector("#city-form");
 var locationInputEl = document.querySelector("#location");
-
+var cityGlobal = " ";
 var containerWeather = document.querySelector("#weather-container");
 
 var loadWeather = function () {
@@ -20,22 +20,98 @@ var formSubmitHandler = function (event) {
 };
 
 var getCityData = function (cityName) {
+  cityGlobal = " ";
   var key = "98cc685a511e4cdddd0311ce79a8e339";
-  fetch(
+  var url =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
-      cityName +
-      " &appid=" +
-      key
-  )
-    .then(function (resp) {
+    cityName +
+    " &appid=" +
+    key;
+
+  fetch(url)
+    .then(resp => {
+      if (!resp.ok) throw new Error(resp.statusText);
       return resp.json();
-    }) // Convert data to json
-    .then(function (data) {
-      console.log(data);
+    })
+    .then(data => {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      cityGlobal = data[0].name;
+      cityWeather(lat, lon, cityGlobal);
     })
     .catch(function (e) {
       console.log(e);
     });
+};
+
+var cityWeather = function (lat, lon, cityName) {
+  var key = "98cc685a511e4cdddd0311ce79a8e339";
+  var lang = "en";
+  var units = "imperial";
+  var url =
+    "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    key +
+    "&units=" +
+    units +
+    "&lang=" +
+    lang;
+  //fetch the weather
+  fetch(url)
+    .then(resp => {
+      if (!resp.ok) throw new Error(resp.statusText);
+      return resp.json();
+    })
+    .then(data => {
+      console.log(data);
+      displayWeather(data, cityName);
+    })
+    .catch(console.err);
+};
+
+var displayWeather = function (cityData, cityName) {
+  containerWeather.style.visibility = "visible";
+  //convert date
+  const date = new Date();
+  var actualDate = date.toLocaleString(cityData.current.dt);
+  //get icon to display
+  var iconWeather =
+    "https://openweathermap.org/img/wn/" +
+    cityData.current.weather[0].icon +
+    ".png";
+
+  $("#cName").html("City: " + cityName + " " + actualDate);
+  $("#cName").append('<img id="imgAttr" src="' + iconWeather + '" />');
+  $("#imgAttr").attr("src", iconWeather);
+  $("#tempCurrent").html(
+    "Temp: " + cityData.current.temp + " <span></span> &deg;F"
+  );
+  $("#windCurrent").html(
+    "Wind: " + cityData.current.wind_speed + " <span></span> MPH"
+  );
+  $("#humidityCurrent").html(
+    "Humidity: " + cityData.current.humidity + " <span></span> %"
+  );
+
+  if (cityData.current.uvi < 3) {
+    $("#uvCurrent").html("UV Index: ");
+    $("#uvCurrent").append(
+      '<span class="uvi badge bg-success">' + cityData.current.uvi + "</span>"
+    );
+  } else if (current.uvi >= 3 && current.uvi < 6) {
+    $("#uvCurrent").html("UV Index: ");
+    $("#uvCurrent").append(
+      '<span class="uvi badge bg-warning>' + cityData.current.uvi + "</span>"
+    );
+  } else if (current.uvi >= 6 && current.uvi < 8) {
+    $("#uvCurrent").html("UV Index: ");
+    $("#uvCurrent").append(
+      '<span class="uvi badge bg-danger">' + cityData.current.uvi + "</span>"
+    );
+  }
 };
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
